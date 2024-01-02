ad8ff12708a950e7d0c4b4b72776bda4089de095
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-02 22:56:31 +0530

    eight commit

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -62,3 +62,12 @@ try {
     console.log(error);
 }
 }
+try {
+    const session = await account.createEmailSession(
+        user.email, user.password
+    )
+    return session
+} catch (error) {
+    console.log(error);
+}
+}
