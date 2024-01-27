0af47c4412c525aad0a1ab3b3722ec5cbdc0d78b
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-27 21:42:30 +0530

    Home-Third commit

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,32 @@
+import {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+const FileUploader = () => {
+    const [fileUrl, setFileurl] = useState('');
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    
+  return (
+    <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+      <input {...getInputProps()} className='cursor-pointer'/>
+      {
+        fileUrl ? (
+         <div>
+            test1
+         </div>
+        ) : (
+            <div>
+                test2
+            </div>
+
+        )
+
+      }
+    </div>
+  )
+}
+
+export default FileUploader
